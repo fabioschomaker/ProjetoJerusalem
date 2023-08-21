d20a09be94a25a62a023453b0bb97b8fc18fce31
Fabio Schomaker Paranhos <132515511+fabioschomaker@users.noreply.github.com>
2023-08-21 19:27:32 -0300

    Código atualizado

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 60)
+});
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icone').onclick = () =>{
+    navbar.classList.toggle('active');
+    
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    
+}
